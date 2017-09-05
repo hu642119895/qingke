4387912c2d27c85a9c850057dc5c1022b653b018
likai <2462924187@qq.com>
2017-09-05 10:06:39 +0800

    firat

---
+++ Scanning/ScanningViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScanningViewController.h
+//  Scanning
+//
+//  Created by qk365_macmini_1 on 2017/9/1.
+//  Copyright © 2017年 yuiiooi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScanningViewController : UIViewController
+
+@end
---
+++ Scanning/ScanningViewController.m
@@ -0,0 +1,37 @@
+//
+//  ScanningViewController.m
+//  Scanning
+//
+//  Created by qk365_macmini_1 on 2017/9/1.
+//  Copyright © 2017年 yuiiooi. All rights reserved.
+//
+
+#import "ScanningViewController.h"
+
+@interface ScanningViewController ()
+
+@end
+
+@implementation ScanningViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
